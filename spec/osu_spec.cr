186b50ff5dcef5e7efebad18ea26cd3d49d16c82
z64 <zachnowicki@gmail.com>
2017-02-04 22:35:40 -0500

    add an initialization test

--- spec/osu_spec.cr
+++ spec/osu_spec.cr
@@ -3,7 +3,8 @@ require "./spec_helper"
 describe Osu do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "initializes" do
+    client = Osu::Client.new ENV["OSU_API_KEY"]
+    client.should be_truthy
   end
 end
