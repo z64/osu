e3e185d7255caa16d4f94d41b17e4f304299f6fb
z64 <zachnowicki@gmail.com>
2017-03-07 22:22:23 -0500

    document Modes, add overloads to Mode.mode

--- src/osu/api/mode.cr
+++ src/osu/api/mode.cr
@@ -3,6 +3,7 @@ module Osu
     module Mode
       extend self
 
+      # NamedTuple of Osu game modes
       Modes = {
         standard: 0,
         taiko:    1,
@@ -10,8 +11,14 @@ module Osu
         mania:    3,
       }
 
-      def mode(index)
+      # Get a game mode ID with a symbol
+      def mode(index : Symbol)
         Modes[index]
+      end
+
+      # Get a game mode symbold from an ID
+      def mode(id : Int32)
+        Modes.to_h.key?(id)
       end
     end
   end
