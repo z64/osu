ba872419c9965609a02960f7e79572938dcb09c0
z64 <zachnowicki@gmail.com>
2017-03-07 20:24:51 -0500

    implement string_converter

--- src/osu/client.cr
+++ src/osu/client.cr
@@ -1,4 +1,5 @@
 require "json"
+require "string_converter"
 require "./mappings/*"
 
 module Osu
--- src/osu/mappings/user.cr
+++ src/osu/mappings/user.cr
@@ -2,23 +2,23 @@ module Osu
   # An Osu user
   class User
     JSON.mapping({
-      user_id:         {type: UInt32, converter: UInt32Converter},
+      user_id:         {type: UInt32?, converter: StringConverter::UInt32},
       username:        String,
-      count300:        {type: UInt32, converter: UInt32Converter},
-      count100:        {type: UInt32, converter: UInt32Converter},
-      count50:         {type: UInt32, converter: UInt32Converter},
-      playcount:       {type: UInt32, converter: UInt32Converter},
-      ranked_score:    {type: UInt64, converter: UInt64Converter},
-      total_score:     {type: UInt64, converter: UInt64Converter},
-      pp_rank:         {type: UInt32, converter: UInt32Converter},
-      level:           {type: Float32, converter: Float32Converter},
-      pp_raw:          {type: Float32, converter: Float32Converter},
-      accuracy:        {type: Float32, converter: Float32Converter},
-      count_rank_ss:   {type: UInt32, converter: UInt32Converter},
-      count_rank_s:    {type: UInt32, converter: UInt32Converter},
-      count_rank_a:    {type: UInt32, converter: UInt32Converter},
+      count300:        {type: UInt32?, converter: StringConverter::UInt32},
+      count100:        {type: UInt32?, converter: StringConverter::UInt32},
+      count50:         {type: UInt32?, converter: StringConverter::UInt32},
+      playcount:       {type: UInt32?, converter: StringConverter::UInt32},
+      ranked_score:    {type: UInt64?, converter: StringConverter::UInt64},
+      total_score:     {type: UInt64?, converter: StringConverter::UInt64},
+      pp_rank:         {type: UInt32?, converter: StringConverter::UInt32},
+      level:           {type: Float32?, converter: StringConverter::Float32},
+      pp_raw:          {type: Float32?, converter: StringConverter::Float32},
+      accuracy:        {type: Float32?, converter: StringConverter::Float32},
+      count_rank_ss:   {type: UInt32?, converter: StringConverter::UInt32},
+      count_rank_s:    {type: UInt32?, converter: StringConverter::UInt32},
+      count_rank_a:    {type: UInt32?, converter: StringConverter::UInt32},
       country:         String,
-      pp_country_rank: {type: UInt32, converter: UInt32Converter},
+      pp_country_rank: {type: UInt32?, converter: StringConverter::UInt32},
       events:          Array(Event),
     })
   end
@@ -26,10 +26,10 @@ module Osu
   class Event
     JSON.mapping({
       display_html:  String,
-      beatmap_id:    {type: UInt32, converter: UInt32Converter},
-      beatmapset_id: {type: UInt32, converter: UInt32Converter},
+      beatmap_id:    {type: UInt32?, converter: StringConverter::UInt32},
+      beatmapset_id: {type: UInt32?, converter: StringConverter::UInt32},
       date:          String,
-      epicfactor:    {type: UInt16, converter: UInt16Converter},
+      epicfactor:    {type: UInt16?, converter: StringConverter::UInt16},
     })
   end
 end
