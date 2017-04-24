def085508db14977cd56ba11f91b1adc4a7ee6c2
z64 <zachnowicki@gmail.com>
2017-04-24 01:36:07 -0400

    use an Enum for API::Mode

--- src/osu/api/mode.cr
+++ src/osu/api/mode.cr
@@ -1,25 +1,11 @@
 module Osu
   module API
-    module Mode
-      extend self
-
-      # NamedTuple of Osu game modes
-      Modes = {
-        standard: 0,
-        taiko:    1,
-        ctb:      2,
-        mania:    3,
-      }
-
-      # Get a game mode ID with a symbol
-      def mode(name : Symbol)
-        Modes[index]
-      end
-
-      # Get a game mode symbold from an ID
-      def mode(id : Int32)
-        Modes.to_h.key?(id)
-      end
+    # Osu game modes
+    enum Mode
+      Standard
+      Taiko
+      Ctb
+      Mania
     end
   end
 end
