806cce46fb6dffa8730c8b85d9d09cf7b4b255d6
z64 <zachnowicki@gmail.com>
2017-04-25 00:05:31 -0400

    add beatmap data mapping

---
+++ src/osu/mappings/beatmap.cr
@@ -0,0 +1,55 @@
+require "string_converter"
+
+module Osu
+  struct Beatmap
+    JSON.mapping({
+      id:               {key: "beatmap_id", type: UInt32?, converter: StringConverter::UInt32},
+      beatmapset_id:    {type: UInt32?, converter: StringConverter::UInt32},
+      approval:         {key: "approved", type: API::Approval, converter: ApprovalConverter},
+      total_length:     {type: UInt32?, converter: StringConverter::UInt32},
+      hit_length:       {type: UInt32?, converter: StringConverter::UInt32},
+      version:          String,
+      md5:              {key: "file_md5", type: String},
+      diff_size:        {type: UInt32?, converter: StringConverter::UInt32, getter: false, setter: false},
+      diff_overall:     {type: UInt32?, converter: StringConverter::UInt32, getter: false, setter: false},
+      diff_approach:    {type: Float64?, converter: StringConverter::Float64, getter: false, setter: false},
+      diff_drain:       {type: UInt32?, converter: StringConverter::UInt32, getter: false, setter: false},
+      mode:             {type: API::Mode, converter: ModeConverter},
+      approved_date:    String,
+      last_update:      String,
+      artist:           String,
+      title:            String,
+      creator:          String,
+      bpm:              {type: UInt32?, converter: StringConverter::UInt32},
+      source:           String,
+      tags:             {type: Array(String), converter: TagsConverter},
+      genre_id:         {type: UInt32?, converter: StringConverter::UInt32},
+      language_id:      {type: UInt32?, converter: StringConverter::UInt32},
+      playcount:        {type: UInt32?, converter: StringConverter::UInt32},
+      passcount:        {type: UInt32?, converter: StringConverter::UInt32},
+      max_combo:        {type: UInt32?, converter: StringConverter::UInt32},
+      difficultyrating: {type: Float64?, converter: StringConverter::Float64, getter: false, setter: false},
+    })
+
+    # The difficulty properties of this map, as a Difficulty struct
+    def difficulty
+      @difficulty ||= Difficulty.new(
+        @diff_size,
+        @diff_overall,
+        @diff_approach,
+        @diff_drain,
+        @difficultyrating
+      )
+    end
+  end
+
+  # Record representing a beatmap's difficulty properties
+  record(
+    Difficulty,
+    size : UInt32?,
+    overall : UInt32?,
+    approach : Float64?,
+    drain : UInt32?,
+    rating : Float64?
+  )
+end
---
+++ src/osu/mappings/converters.cr
@@ -0,0 +1,22 @@
+module Osu
+  # :nodoc:
+  macro offset_enum_converter(kind, offset)
+  struct {{kind.id}}Converter
+    def self.from_json(parser)
+      index = Int32.new parser.read_string
+
+      API::{{kind.id}}.new index + {{offset}}
+    end
+  end
+end
+
+  offset_enum_converter Mode, 0
+  offset_enum_converter Approval, 2
+
+  # :nodoc:
+  struct TagsConverter
+    def self.from_json(parser)
+      parser.read_string.split " "
+    end
+  end
+end
